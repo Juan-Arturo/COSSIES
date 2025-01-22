825db2349c160fe18d58a4456e047edf71c1bfdd
Juan-Arturo <jagpjagp002@gmail.com>
2025-01-22 11:35:54 -0600

    creacion de componentes compartidos y enrutamiento avanzado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { PanelComponent } from './main/panel/panel.component';
+import { HomeComponent } from './main/panel/home/home.component';
+import { ContactComponent } from './main/panel/contact/contact.component';
+import { AboutComponent } from './main/panel/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "panel", pathMatch: "full" },
+
+    //Panel principal
+  {
+    path: "panel",
+    component: PanelComponent,
+    children: [
+      { path: "", redirectTo: "home", pathMatch: "full" }, 
+      { path: "home", component: HomeComponent }, 
+      { path: "contact", component: ContactComponent }, 
+      { path: "APOD", component: AboutComponent }, 
+      { path: "**", redirectTo: "home" }, 
+    ],
+  },
+
+    {path: "**", component: PanelComponent},
+
+];
