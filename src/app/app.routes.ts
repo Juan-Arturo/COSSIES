848ca1a862a382bcdb9e446a256f9cb3abe830ac
Juan-Arturo <jagpjagp002@gmail.com>
2025-01-27 10:17:12 -0600

    componente welcome

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,15 @@ import { PanelComponent } from './main/panel/panel.component';
 import { HomeComponent } from './main/panel/home/home.component';
 import { ContactComponent } from './main/panel/contact/contact.component';
 import { AboutComponent } from './main/panel/about/about.component';
+import { WelcomeComponent } from './main/welcome/welcome.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "panel", pathMatch: "full" },
+     
+    
+    //Vista de bienvenida
+  { path: "welcome", component: WelcomeComponent }, 
+
 
     //Panel principal
   {
@@ -19,6 +25,8 @@ export const routes: Routes = [
       { path: "**", redirectTo: "home" }, 
     ],
   },
+
+
 
     {path: "**", component: PanelComponent},
 
